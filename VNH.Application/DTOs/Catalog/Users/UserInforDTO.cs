f7d508f7ffaee5b6deddf3fcd047243569af869c
LuongXuanNhat <xuannhat1932002@gmail.com>
2023-09-26 09:01:47 +0700

    #12 | Add image to Database (test)

---
+++ VNH.Application/DTOs/Catalog/Users/UserInforDTO.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using VNH.Domain.Enums;
+
+namespace VNH.Application.DTOs.Catalog.Users
+{
+    public class UserInforDTO
+    {
+        [StringLength(100)]
+        public string Fullname { get; set; }
+        [Column(TypeName = "datetime")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? DateOfBirth { get; set; }
+        [StringLength(50)]
+        public Gender Gender { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string PhoneNumber { get; set; }
+    }
+}
---
+++ VNH.Application/Mappers/UserMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using VNH.Application.DTOs.Catalog.Users;
+using VNH.Domain;
+
+namespace VNH.Application.Mappers
+{
+    public class UserMapper : Profile
+    {
+        public UserMapper() {
+            CreateMap<User, UserInforDTO>().ReverseMap();
+        }
+    }
+}
