f6e2a404e6bac379a75de3252d8321b38c383a55
vuanh25 <chimcudammua@gmail.com>
2023-12-10 10:01:07 +0700

    update

---
+++ VNH.Domain/Entities/MultipleChoice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VNH.Domain.Entities
+{
+
+    [Table("ListQuiz")]
+    public partial class MultipleChoice
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+        [StringLength(500)]
+
+        public string Description { get; set; }
+        [StringLength(500)]
+
+        public string Author { get; set; }
+        [StringLength(500)]
+
+
+
+    }
+}
